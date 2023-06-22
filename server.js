bb8dbc2e300202eb124a2243d4c2e2317928c0a6
Muhammad Fateh Mehmood <fatehmehmood123@gmail.com>
2023-06-22 15:46:55 +0500

    Count

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const io = require('socket.io')(3000);
-users = {};
+const io = require('socket.io')(3000,{
+    cors:{
+        origin:["https://fatehmehmood123.github.io","http://localhost:5500"]
+    }
+});
+var onlineCount = 1;
+let users = {};
 io.on('connection', socket =>{
     socket.on('user-added',userName=>{
         users[socket.id]=userName;
-        socket.broadcast.emit('user-joined',userName);
+        onlineCount++;
+        socket.broadcast.emit('user-joined',userName,onlineCount);
     });
     socket.on('send-chat-message',(message)=>{
         socket.broadcast.emit("chat-message",message,users[socket.id]);
     });
     socket.on('disconnect', ()=>{
-        socket.broadcast.emit('disconnected',users[socket.id]);
+        onlineCount--;
+        socket.broadcast.emit('disconnected',users[socket.id],onlineCount);
         delete users[socket.id]
     });
 });
